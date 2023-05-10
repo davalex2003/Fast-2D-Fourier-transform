94b974c14ffeb562ab421b890472a05b4456a81a
davalex2003 <97729979+davalex2003@users.noreply.github.com>
2023-05-10 19:39:56 +0300

    1000-7

--- new_fft.m
+++ new_fft.m
@@ -16,7 +16,7 @@ function ans = new_fft(n)
         ans(i, j) = value_first(i,j) + value_second(i,j)*W(log2(size(n,1)),j - 1) + value_thirst(i,j)*W(log2(size(n,1)),i - 1) + value_last(i,j)*W(log2(size(n,1)),i+j - 2);
         ans(i + size(n,1)/2, j) = value_first(i,j) + value_second(i,j)*W(log2(size(n,1)),j - 1) - value_thirst(i,j)*W(log2(size(n,1)),i - 1) - value_last(i,j)*W(log2(size(n,1)),i+j - 2);
         ans(i, j + size(n,1)/2) = value_first(i,j) - value_second(i,j)*W(log2(size(n,1)),j - 1) + value_thirst(i,j)*W(log2(size(n,1)),i - 1) - value_last(i,j)*W(log2(size(n,1)),i+j - 2);
-        ans(i + size(n,1)/2, j + size(n,1)/2) = value_first(i,j) - value_second(i,j)*W(log2(size(n,1)),j- 1) - value_thirst(i,j)*W(log2(size(n,1)),i - 1) + value_last(i,j)*W(size(n,1),i+j - 2);
+        ans(i + size(n,1)/2, j + size(n,1)/2) = value_first(i,j) - value_second(i,j)*W(log2(size(n,1)),j - 1) - value_thirst(i,j)*W(log2(size(n,1)),i - 1) + value_last(i,j)*W(log2(size(n,1)),i+j - 2);
       endfor
     endfor
   end
