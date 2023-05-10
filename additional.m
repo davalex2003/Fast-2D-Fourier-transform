09c17ba52b1c957cd29ceaa08a46f548961d9b24
davalex2003 <97729979+davalex2003@users.noreply.github.com>
2023-05-10 17:45:56 +0300

    Update additional.m

--- additional.m
+++ additional.m
@@ -2,7 +2,7 @@ function retval = additional(matr)
   retval = matr;
   for i = 1:size(matr,1)
     for j = 1:size(matr,2)
-      retval(i,j) = matr(1,1)*W(1, i)*W(1,j) + matr(1,2)*W(1, i)*W(1,j) + matr(2,1)*W(1, i)*W(1,j) + matr(2,2)*W(1, i)*W(1,j);
+      retval(i,j) = matr(1,1) + matr(1,2)*W(1, i) + matr(2,1)*W(1, j) + matr(2,2)*W(1, i)*W(1, j);
     endfor
   endfor
 endfunction
