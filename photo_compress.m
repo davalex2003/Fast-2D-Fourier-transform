6d8d55b49e535c57aa804721570633c84e3aa51f
davalex2003 <97729979+davalex2003@users.noreply.github.com>
2023-05-12 23:45:36 +0300

    Облик твой

--- photo_compress.m
+++ photo_compress.m
@@ -1,9 +1,22 @@
-A = imread('graphics.jpg');
-B = rgb2gray(A);
-Bt = fft2(double(B));
-Btsort = sort(abs(Bt(:)));
-thresh = Btsort(floor((0.98)*length(Btsort)));
-ind = abs(Bt)>thresh;
-Atlow = Bt.*ind;
-Alow = uint8(real(ifft2(Atlow)));
-imshow(Alow)
+function photo_compress(name, k)
+  A = imread(name);
+  B = rgb2gray(A);
+  transpose = false;
+  if (size(B,1) == size(B,2))
+      Bt = fft2(double(B));
+  elseif (size(B,1) > size(B,2))
+      transpose = true;
+      Bt = square(double(B));
+  else
+      Bt = square(double(B));
+  end
+  Btsort = sort(abs(Bt(:)));
+  thresh = Btsort(floor((k)*length(Btsort)));
+  ind = abs(Bt)>thresh;
+  Atlow = Bt.*ind;
+  Alow = uint8(real(ifft2(Atlow)));
+  if (transpose)
+    Alow = rot90(Alow, -1);
+  end
+  imshow(Alow);
+end
