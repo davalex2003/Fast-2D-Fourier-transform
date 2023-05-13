55a0dc6d372274bd6e0a55ae8d25c3f1d7b57826
davalex2003 <97729979+davalex2003@users.noreply.github.com>
2023-05-13 20:31:30 +0300

    Шилов<3

--- main.m
+++ main.m
@@ -1,2 +1,26 @@
 clear; clc;
-photo_compress('USSR.jpg', 0.99999);
+disp('Выберите нужный вариант:');
+disp('1. Сжатие фотографии');
+disp('2. Размер рандомного массива');
+disp('3. Экспорт массива из .csv файла');
+disp('4. Ввод массива вручную');
+number = input('Введите число: ');
+while (number != 1 && number != 2 && number != 3 && number != 4)
+  number = input('Введите число: ');
+end
+if (number == 1)
+  photo = input('Введите путь и название файла: ', 's');
+  try
+    A = imread(photo);
+    k = input('Введите степень сжатия: ');
+    photo_compress(photo, k);
+  catch exception
+    disp('Ошибка при открытии фотографии.');
+  end
+elseif (number == 2)
+
+elseif (number == 3)
+
+else (number = 4);
+
+end
