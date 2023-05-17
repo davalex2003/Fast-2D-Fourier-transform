ce6dbd77c7210b5acd8624b6559e5afe2894c30f
nikita-kuprin <47470988+nikita-kuprin@users.noreply.github.com>
2023-05-17 12:25:24 +0300

    Final Version

--- W.m
+++ W.m
@@ -1,3 +1,4 @@
-function retval = W(s, y)
-  retval = exp((-2*pi*y*i)/(2^s));
+% Функция для вычисления коэффициента-бабочки
+function butterfly_coefficient = W(s, y)
+  butterfly_coefficient = exp((-2*pi*y*i)/(2^s));
 endfunction
