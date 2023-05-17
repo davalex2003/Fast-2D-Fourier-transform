251bdc7126ac6558aa851e53affbfaa428cde30d
nikita-kuprin <47470988+nikita-kuprin@users.noreply.github.com>
2023-05-17 12:22:53 +0300

    Final Version

--- additional.m
+++ additional.m
@@ -1,11 +1,22 @@
-function retval = additional(matr)
-  retval = matr;
-  for i = 1:size(matr,1)
-    for j = 1:size(matr,2)
-      retval(i,j) = matr(1,1) + matr(1,2)*W(1, i) + matr(2,1)*W(1, j) + matr(2,2)*W(1, i)*W(1, j);
+% Функция для подсчёта Фурье-образа матрицы размера 2 на 2 по определению
+function fourier_image = additional(matr)
+  % Инициализируем нулями возвращаемую матрицу.
+  fourier_image = zeros(2, 2);
+  
+  % Предварительное вычисление W(1, i).
+  W_i = W(1, 1:2);
+  % Предварительное вычисление W(1, j).
+  W_j = W(1, 1:2);
+  
+  % Основной цикл вычисления Фурье-образа.
+  % Используем цикл parfor, который позволяет разбить независимые 
+  % друг от друга вычисления на процессы, вычисляемые параллельно.
+  parfor i = 1:2
+    for j = 1:2
+      fourier_image(i,j) = matr(1,1) + matr(1,2)*W_i(i) + matr(2,1)*W_j(j) + matr(2,2)*W_i(i)*W_j(j);
     endfor
-  endfor
-  tmp = retval(1,1);
-  retval(1,1) = retval(2,2);
-  retval(2,2) = tmp;
+  end
+  tmp = fourier_image(1,1);
+  fourier_image(1,1) = fourier_image(2,2);
+  fourier_image(2,2) = tmp;
 endfunction
